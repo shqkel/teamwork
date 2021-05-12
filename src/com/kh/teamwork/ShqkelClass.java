01c0060d84c5b4bce083fe421fbcc4ed5f7355d6
shqkel <shqkel1863@gmail.com>
2021-05-12 20:19:32 +0900

    bar메소드로 수정!

--- src/com/kh/teamwork/ShqkelClass.java
+++ src/com/kh/teamwork/ShqkelClass.java
@@ -2,8 +2,8 @@ package com.kh.teamwork;
 
 public class ShqkelClass {
 
-	public void hello() {
-		System.out.println("hello teamwork!");
+	public void bar() {
+		System.out.println("hello baaaaaaaaaaaaaaaaar!");
 	}
 	
 }
